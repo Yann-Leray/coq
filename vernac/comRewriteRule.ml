1825cbb5569220d9dd9c5b9dedb08fc2bc1f2c86
Yann Leray <yann.leray@inria.fr>
2024-07-26 14:30:02 +0200

    Better warning messages for rewrite rules

--- vernac/comRewriteRule.ml
+++ vernac/comRewriteRule.ml
@@ -384,7 +384,7 @@ let rewrite_rules_break_SR_msg = CWarnings.create_msg rewrite_rules_break_SR_war
 let warn_rewrite_rules_break_SR ~loc reason =
   CWarnings.warn rewrite_rules_break_SR_msg ?loc reason
 let () = CWarnings.register_printer rewrite_rules_break_SR_msg
-  (fun reason -> Pp.(str "This rewrite rule breaks subject reduction (" ++ reason ++ str ")."))
+  (fun reason -> Pp.(str "This rewrite rule breaks subject reduction " ++ reason))
 
 let interp_rule (udecl, lhs, rhs: Constrexpr.universe_decl_expr option * _ * _) =
   let env = Global.env () in
@@ -481,15 +481,16 @@ let interp_rule (udecl, lhs, rhs: Constrexpr.universe_decl_expr option * _ * _) 
   let flags = { Pretyping.no_classes_no_fail_inference_flags with patvars_abstract = true } in
   let evd', rhs =
     try Pretyping.understand_tcc ~flags env evd ~expected_type:(OfType typ) rhs
-    with Type_errors.TypeError _ | Pretype_errors.PretypeError _ ->
-      warn_rewrite_rules_break_SR ~loc:rhs_loc (Pp.str "the replacement term doesn't have the type of the pattern");
+    with Pretype_errors.PretypeError (env, evd, e) ->
+      warn_rewrite_rules_break_SR ~loc:rhs_loc
+        Pp.(str "(the replacement term doesn't have the type of the pattern)." ++ fnl () ++ Himsg.explain_pretype_error env evd e);
       Pretyping.understand_tcc ~flags env evd rhs
   in
 
   let evd' = Evd.minimize_universes evd' in
   let _qvars', uvars' = EConstr.universes_of_constr evd' rhs in
   let evd' = Evd.restrict_universe_context evd' (Univ.Level.Set.union uvars uvars') in
-  let fail pp = warn_rewrite_rules_break_SR ~loc:rhs_loc Pp.(str "universe inconsistency, missing constraints: " ++ pp) in
+  let fail pp = warn_rewrite_rules_break_SR ~loc:rhs_loc Pp.(str "(universe inconsistency)." ++ spc () ++ str"Missing constraints: " ++ pp) in
   let () = UState.check_uctx_impl ~fail (Evd.ustate evd) (Evd.ustate evd') in
   let evd = evd' in
 
